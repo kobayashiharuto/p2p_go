1d9de7e45875b80fca79b106c15bb49ff20ce4b6
kl <datesaver1234@gmail.com>
2022-01-05 00:45:19 +0900

    tcp

--- test.go
+++ test.go
@@ -2,19 +2,26 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/libp2p/go-libp2p"
 )
 
 func main() {
-	// start a libp2p node with default settings
-	node, err := libp2p.New()
+	node, err := libp2p.New(
+		libp2p.ListenAddrStrings("/ip4/127.0.0.1/tcp/2000"),
+	)
 	if err != nil {
 		panic(err)
 	}
-
-	// print the node's listening addresses
 	fmt.Println("Listen addresses:", node.Addrs())
+	// wait for a SIGINT or SIGTERM signal
+	ch := make(chan os.Signal, 1)
+	signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM)
+	<-ch
+	fmt.Println("Received signal, shutting down...")
 
 	// shut the node down
 	if err := node.Close(); err != nil {
